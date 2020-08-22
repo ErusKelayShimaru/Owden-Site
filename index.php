66da08ece15be6326f9cbf29e0fbf8b3cc119345
ErusKelayShimaru <eruskelay@gmail.com>
2020-08-22 13:54:02 -0300

    Troca do arquivo index.html pelo arquivo ficha.php e inserção do arquivo index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+	
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Ficha Lado A</title>
+		<link rel="stylesheet" type="text/css" href="css/estilogeral.css"/>
+		<link rel="shortcut icon" href="img/favicon.ico" />
+		
+		<style>
+
+		</style>
+		
+		<script>
+		
+		</script>
+	</head>
+	
+	<body>
+		<header>
+		
+		</header>
+		
+		<section id="fichaA">
+			<canvas id="telaFichaA" width="1412" height="2000">Este navegador não suporta Canvas</canvas>
+		</section>
+		
+		<footer>
+
+		</footer>
+	</body>
+</html>
