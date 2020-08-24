0f3a13a9781755fdca4f0f12a4b43018e4b78256
ErusKelayShimaru <eruskelay@gmail.com>
2020-08-24 13:06:36 -0300

    Criado o arquiv novopersonagem.php para fazer a página de criação de fichas.

---
+++ novopersonagem.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+	
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Owden Antidiluvianis</title>
+		<link rel="stylesheet" type="text/css" href="css/estilo.css"/>
+		<link rel="shortcut icon" href="img/favicon.ico" />
+		
+		<style>
+
+		</style>
+
+		<script>
+		
+		</script>
+	</head>
+	
+	<body>
+		<header>
+		
+		</header>
+		
+		<a href="ficha.php">Abrir a ficha</a>
+		
+		<footer>
+
+		</footer>
+	</body>
+</html>
